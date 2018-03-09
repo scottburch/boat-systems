f8be3100f02fe258c7f0ffb86eab7ca8152b7021
scott <scott@bulldoginfo.com>
2018-03-08 16:51:54 -0800

    Added firmware code

--- test/networkTest.js
+++
@@ -1,6 +0,0 @@
-const receiver = require('../network/libs/NetworkReceiver').createNetworkReceiver();
-
-receiver.on('listening', (...args) => console.log('listening', args));
-receiver.on('message', (msg) => console.log('message', msg));
-receiver.on('error', (...args) => console.log('error', args));
-
