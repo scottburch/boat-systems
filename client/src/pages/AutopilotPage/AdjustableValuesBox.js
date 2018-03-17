bf0f4e75baf57cb1d9c99f71a76094b2c40366a6
scott <scott@bulldoginfo.com>
2018-03-16 23:15:30 -0700

    increase rudder mult increment

--- client/src/pages/AutopilotPage/AdjustableValuesBox.js
+++ client/src/pages/AutopilotPage/AdjustableValuesBox.js
@@ -18,7 +18,7 @@ const eventToString = require('key-event-to-string')({
 const adjustableValues = [
     {key: 'rudderTime', text: 'RudderTime', inc: 10},
     {key: 'rudderWait', text: 'Rudder Wait', inc: 10},
-    {key: 'rudderMult', text: 'Rudder mult', inc: 0.5},
+    {key: 'rudderMult', text: 'Rudder mult', inc: 5},
     {key: 'smoothing', text: 'Smoothing', inc: 10},
     {key: 'kP', text: 'P', inc: 0.05},
     {key: 'kI', text: 'I', inc: 0.01},
