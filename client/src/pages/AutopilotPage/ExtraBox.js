d7e358bdab5449bf3baa6a5cc90df50e42632358
scott <scott@bulldoginfo.com>
2018-03-23 09:55:06 -0700

    fixed loop in display

--- client/src/pages/AutopilotPage/ExtraBox.js
+++ client/src/pages/AutopilotPage/ExtraBox.js
@@ -4,7 +4,6 @@ const utils = require('../../../../autopilot/utils');
 import {Component} from "../../components/Component";
 import {values} from "../../stores/AutopilotClientStore";
 import {observer} from 'mobx-react'
-import {autorun, extendObservable} from 'mobx'
 import {Maybe} from 'simple-monads'
 
 @observer
@@ -13,34 +12,9 @@ class ExtraBox extends Component {
     constructor(props) {
         super(props);
         values.get('voltage') || values.set('voltage', 1023);
-        setInterval(() => this.setState({minHz: 999999}), 5000);
-
-        this.stop = autorun(() => {
-
-            const [hz, delay] = [
-                values.get('hz'),
-                values.get('compassDelay')
-            ];
-            this.mounted ? (
-                this.setState({
-                    minHz: Math.min(values.get('hz'), this.state.minHz),
-                    maxDly: isNaN(this.state.maxDly) ? 0 : Math.max(delay, this.state.maxDly),
-                })
-            ) : (
-                this.state = {
-                    minHz: 999999,
-                    maxDly: 0
-                }
-            )
-        })
-    }
-
-    componentDidMount() {
-        this.mounted = true;
-    }
-
-    componentWillUnmount() {
-        this.stop();
+        setInterval(() => this.minHz = 999999, 5000);
+        this.maxDly = 0;
+        this.minHz = 999999;
     }
 
     convertToVolts(num) {
@@ -49,19 +23,21 @@ class ExtraBox extends Component {
 
 
     render() {
+        this.maxDly = isNaN(this.maxDly) ? 0 : Math.max(values.get('compassDelay'), this.maxDly);
+        this.minHz = Math.min(values.get('hz'), this.minHz);
         return (
             <div>
                 <ValueField label="Yaw Speed"><Color>{values.get('yawSpeed')}</Color></ValueField>
                 <ValueField label="Roll"><Color>{values.get('roll')}</Color></ValueField>
                 <ValueField label="Pitch"><Color>{values.get('pitch')}</Color> </ValueField>
                 <ValueField label="HZ">{values.get('hz')}</ValueField>
-                <ValueField label="Min HZ">{this.state.minHz}</ValueField>
+                <ValueField label="Min HZ">{this.minHz}</ValueField>
                 <ValueField label="R State">{values.get('rudderState')}</ValueField>
                 <ValueField label="Base">{values.get('prevBase')}</ValueField>
                 <ValueField label="Tach">{values.get('prevTach')}</ValueField>
                 <ValueField label="Speed">{values.get('prevSpeed')}</ValueField>
                 <ValueField label="Cmps Dly">{values.get('compassDelay')}</ValueField>
-                <ValueField label="Max Dly">{this.state.maxDly}</ValueField>
+                <ValueField label="Max Dly">{this.maxDly}</ValueField>
                 <ValueField label="Voltage">{this.convertToVolts(values.get('volts'))} ({this.convertToVolts(values.get('minVolts'))}-{this.convertToVolts(values.get('maxVolts'))})</ValueField>
             </div>
         )
