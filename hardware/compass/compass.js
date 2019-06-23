404eb0e78fa40ee3b8214017d3a46b843c21bf27
scott <scott@bulldoginfo.com>
2019-06-23 15:02:13 -0700

    compass cleanup and add compass auto-calibration command

--- hardware/compass/compass.js
+++ hardware/compass/compass.js
@@ -1,8 +1,8 @@
 const i2c = require('i2c-bus');
 const {sendError, sendInfo} = require('../../network/logSender');
 const {sendMessage} = require('../../network/networkBus');
-const utils = require('../rudderInterface/utils');
 
+const AUTO_CALIBRATION = [0x98, 0x95, 0x99, 0x93];
 const CMPS14_ADDR = 0x60;
 const BEARING = 0x02;
 const ROLL = 0x05;
@@ -11,6 +11,7 @@ const CALIBRATION_STATE = 0x1e;
 
 const i2c1 = i2c.openSync(1);
 
+
 sendInfo('CMPS-14 compass running');
 
 sendInfo(`Magnetometer calibration ${i2c1.readByteSync(CMPS14_ADDR, CALIBRATION_STATE) & 3}`);
@@ -18,6 +19,11 @@ sendInfo(`Accelerometer calibration ${(i2c1.readByteSync(CMPS14_ADDR, CALIBRATIO
 sendInfo(`Gyro calibration state ${(i2c1.readByteSync(CMPS14_ADDR, CALIBRATION_STATE) & 0x30) >> 4}`);
 sendInfo(`Compass calibration state ${(i2c1.readByteSync(CMPS14_ADDR, CALIBRATION_STATE) & 0xc0) >> 6}`);
 
+
+const writeCommand = (bytes) => {
+        i2c1.writeByteSync(CMPS14_ADDR, 0x00, bytes.shift());
+        bytes.length && setTimeout(() => writeCommand(bytes), 20);
+};
 
 const loop = () => {
     sendMessage('AHRS', {
@@ -32,12 +38,13 @@ const loop = () => {
 const readWord = (register) => {
     const high = i2c1.readByteSync(CMPS14_ADDR, register);
     const low = i2c1.readByteSync(CMPS14_ADDR, register + 1);
-    return utils.fixed((high * 256 + low)/10, 1);
+    return (((high << 8) | low)/10).toFixed(1);
 };
 
-const readSigned = (register) => {
-    let value = i2c1.readByteSync(CMPS14_ADDR, register);
-    return value & 0x80 ? value - 256 : value;
-};
+const readSigned = (register) =>
+    new Int8Array([i2c1.readByteSync(CMPS14_ADDR, register)])[0];
+
+
 loop();
+writeCommand(AUTO_CALIBRATION);
 
