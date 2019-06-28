d2a842aca3a3429ca858130fb046c18cdee2ed11
scott <scott@bulldoginfo.com>
2019-06-28 06:40:20 -0700

    changed websocket connection to use window.location.hostname so it is automatic

--- client-cra/src/services/communicationService.js
+++ client-cra/src/services/communicationService.js
@@ -1,11 +1,11 @@
 import {values} from "../stores/AutopilotClientStore";
 
-const ws = new WebSocket(`ws://localhost:3001/ws`);
+const ws = new WebSocket(`ws://${window.location.hostname}:3001/ws`);
 
 ws.onmessage = (msg) => {
     const messageObj = JSON.parse(msg.data);
     messageObj.event === 'AUTOPILOT' && values.merge(messageObj.data)
-}
+};
 
 ws.onopen = () =>
     ws.send(JSON.stringify({cmd: 'register', event: 'AUTOPILOT'}));
