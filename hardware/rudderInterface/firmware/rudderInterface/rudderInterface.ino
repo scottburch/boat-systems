e1e20900d1b9544a946178235e370070724f3d60
scott <scott@bulldoginfo.com>
2019-07-12 23:03:13 -0700

    cleaned up the firmware - set base to 0 to try to keep the rudder from going crazy

--- hardware/rudderInterface/firmware/rudderInterface/rudderInterface.ino
+++ hardware/rudderInterface/firmware/rudderInterface/rudderInterface.ino
@@ -6,7 +6,7 @@ int MOTOR_DIRECTION_PIN = 10;
 int MOTOR_SPEED_PIN = 11;
 
 // Analog pins
-int MOTOR_TACH_PIN = 0;
+int MOTOR_TACH_PIN = A0;
 
 int speed = 0;
 int base;
@@ -34,17 +34,9 @@ void setupComms() {
 }
 
 void receiveData(int byteCount){
-    Serial.print("received ");
-    Serial.print(byteCount);
-    Serial.println(" bytes");
-
     byte command = Wire.read();
 
-    Serial.print("cmd: ");
-    Serial.println(command);
-
     if(command == SET_RUDDER_CMD) {
-            Serial.println("SET_RUDDER_CMD");
             byte low = Wire.read();
             byte high = Wire.read();
             setRudder(low | (high << 8));
@@ -62,20 +54,20 @@ void receiveData(int byteCount){
     }
 }
 
-void setRudder(int speed) {
+void setRudder(int s) {
+    speed = s;
+    Serial.print("Rudder: ");
     Serial.println(speed);
     digitalWrite(MOTOR_DIRECTION_PIN, speed > 0 ? LOW : HIGH);
     if(speed == 0) {
         analogWrite(MOTOR_SPEED_PIN, 0);
     }
-    base = 100;
+    base = 0;
 }
 
 
 // callback for sending data
 void sendData(){
-    Serial.print("sending: ");
-    Serial.println(cmdResponse);
   Wire.write(cmdResponse);
 }
 
@@ -87,10 +79,11 @@ void sendData(){
 
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(57600);
     setupComms();
     // Set PCM frequency
-  TCCR1B = (TCCR1B & 0b11111000) | 0x01;
+//  TCCR1B = (TCCR1B & 0b11111000) | 0x01;
+  TCCR2B = (TCCR2B & 0b11111000) | 0x01;
 
   pinMode(MOTOR_SPEED_PIN, OUTPUT);
   pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
